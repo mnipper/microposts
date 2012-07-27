4156f996e672818af89b160e5bfb4245b887bd57
Michael Nipper <mjn4406@gmail.com>
2012-07-26 21:48:20 -0400

    Finish the signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
 
   validates :name, :presence => true, :length => { :maximum => 50 }
   validates :email, :presence => true, :uniqueness => { :case_sensitive => false }, :format => { :with => /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i  }
-  validates :password, :presence => true, :length => { :minimum => 6 }
+  validates :password, :length => { :minimum => 6 }
   validates :password_confirmation, :presence => true
 end
